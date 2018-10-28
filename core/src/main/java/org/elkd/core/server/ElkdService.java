743cfc9fbfb310989e13ff6624a18d9a218e37c4
djjonno <jonathon.scanes@gmail.com>
2018-10-28 14:17:09 -0700

    Add RPC models AppendEntries, RequestVotes to .proto

--- core/src/main/java/org/elkd/core/server/ElkdService.java
+++ core/src/main/java/org/elkd/core/server/ElkdService.java
@@ -4,7 +4,12 @@ import io.grpc.stub.StreamObserver;
 
 public class ElkdService extends ElkdServiceGrpc.ElkdServiceImplBase {
   @Override
-  public StreamObserver<HelloRequest> sayHello(final StreamObserver<HelloReply> responseObserver) {
-    return super.sayHello(responseObserver);
+  public void appendEntries(AppendEntriesRequest request, StreamObserver<AppendEntriesResponse> responseObserver) {
+    super.appendEntries(request, responseObserver);
+  }
+
+  @Override
+  public void requestVotes(RequestVotesRequest request, StreamObserver<RequestVotesResponse> responseObserver) {
+    super.requestVotes(request, responseObserver);
   }
 }
