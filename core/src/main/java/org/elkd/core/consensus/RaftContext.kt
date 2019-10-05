869e85c66865fa8d4b1df4759aa7de4c580c5017
djjonno <djjonno@amazon.com>
2019-10-05 12:27:18 -0700

    Add synchronize read/writes on RaftContext

--- core/src/main/java/org/elkd/core/consensus/RaftContext.kt
+++ core/src/main/java/org/elkd/core/consensus/RaftContext.kt
@@ -1,24 +1,24 @@
 package org.elkd.core.consensus
 
-import org.elkd.shared.annotations.Mockable
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
+import javax.annotation.concurrent.ThreadSafe
 
-@Mockable
-class RaftContext {
-  /* persistent state */
-  private var _currentTerm: Int = 0
-  private var _votedFor: String? = null
+@ThreadSafe class RaftContext {
+  private var _currentTerm: AtomicInteger = AtomicInteger(0)
+  private var _votedFor: AtomicReference<String> = AtomicReference("")
 
   var currentTerm: Int
-    get() = _currentTerm
+    get() = _currentTerm.get()
     set(currentTerm) {
-      _currentTerm = currentTerm
+      _currentTerm.set(currentTerm)
       commit()
     }
 
   var votedFor: String?
-    get() = _votedFor
+    get() = _votedFor.get()
     set(votedFor) {
-      _votedFor = votedFor
+      _votedFor.set(votedFor)
       commit()
     }
 
